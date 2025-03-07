73dec010be34915010f1c7bdc0c3cda536513024
Kaley-Schlimgen <kaleynicole89@gmail.com>
2025-03-07 00:18:23 -0700

    commit 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,14 +8,17 @@ public class Main {
 
     public static void main(String[] args) {
         Thread firstThread = new Thread(new IncrementThread());
+        Thread secondThread = new Thread(new DecrementThread());
         logger.setLevel(Level.INFO);
 
         firstThread.start();
+        secondThread.start();
 
         System.out.println("Counting Up:");
 
         try {
             firstThread.join();
+            secondThread.join();
         } catch (InterruptedException e) {
                 logger.log(Level.SEVERE, "Main thread error", e);
         }
@@ -35,6 +38,22 @@ class IncrementThread implements Runnable {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 logger.log(Level.WARNING, "Counting up thread error", e);
+            }
+        }
+    }
+}
+
+class DecrementThread implements Runnable {
+    private static final Logger logger = Logger.getLogger(DecrementThread.class.getName());
+
+    @Override
+    public void run() {
+        for (int i = 20; i >= 0; i--) {
+            System.out.println(i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                logger.log(Level.WARNING, "Counting down thread error", e);
             }
         }
     }
